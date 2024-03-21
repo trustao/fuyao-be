117d695de697778315e590d3ae7ea7aeebfff99b
taorui <taorui@p1.com>
2024-03-21 12:32:38 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import Koa from 'koa';
 import bodyParser from '@koa/bodyparser';
+import cors from 'koa-cors';
 import logger from "./util/logger";
 import router from "./routes";
 import './sync-script';
@@ -19,6 +20,7 @@ const app = new Koa();
 const host = process.env.APP_HOST || '0.0.0.0';
 const port = Number(process.env.APP_PORT) || 3000
 
+app.use(cors());
 app.use(logTime)
 app.use(catchError)
 app.use(IpAccessControl)
