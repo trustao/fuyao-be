8337307afbdb966b760b4bb0ee151b030b1ead91
taorui <taorui@p1.com>
2024-03-22 18:41:36 +0800

    log

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,3 +1,13 @@
+import moment from "moment";
 
 
-export default console
+function prefix() {
+  return `[FY]-[${moment().format('YYYY-MM-DD HH:mm:ss')}]`;
+}
+
+export default {
+  log: (...args: any[]) => console.log(prefix(), ...args),
+  error: (...args: any[]) => console.error(prefix(), ...args),
+  warn: (...args: any[]) => console.warn(prefix(), ...args),
+  info: (...args: any[]) => console.info(prefix(), ...args),
+}
