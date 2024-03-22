8fa1609a1f80dca70c079656103af2c616a19bb9
taorui <taorui@p1.com>
2024-03-22 19:16:26 +0800

    log ip

--- src/middlewave/log.ts
+++ src/middlewave/log.ts
@@ -4,7 +4,6 @@ import logger from "../util/logger";
 
 export const logTime = async (ctx: Context, next: Next) => {
   const now = Date.now();
-  const ip = ctx.ips.length > 0 ? ctx.ips[ctx.ips.length - 1] : ctx.ip;
   await next();
-  logger.log(`[M:${ctx.method}] [I:${ip}] ${ctx.url} ${Date.now() - now}ms`);
+  logger.log(`[M:${ctx.method}] [I:${ctx.ip}] [Ips: ${ctx.ips}] ${ctx.url} ${Date.now() - now}ms`);
 }
